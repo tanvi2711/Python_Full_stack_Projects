a161d2cf5a15eb4fa60db6eda58ffc8008a4644b
Tanvi Jivatode <143626918+tanvi2711@users.noreply.github.com>
2026-01-18 23:55:49 +0530

    Update settings.py

--- EcomApp/EcomApp/settings.py
+++ EcomApp/EcomApp/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -125,6 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/upload/'
 MEDIA_ROOT = BASE_DIR/'upload'
+
 
 
 
