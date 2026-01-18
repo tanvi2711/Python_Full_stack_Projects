64c82f1108cbb75b94989e4e75f2ed1c52bbe1fc
Tanvi Jivatode <143626918+tanvi2711@users.noreply.github.com>
2026-01-18 23:56:06 +0530

    Update urls.py

--- EcomApp/EcomApp/urls.py
+++ EcomApp/EcomApp/urls.py
@@ -21,6 +21,7 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Ecom.urls'))
+    path('',include('Ecom.urls')),
 ]
+
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
