d375464663e3afdcf26066a5b78f92cee8beccdb
Tanvi Jivatode <tanvijivatode2711@gmail.com>
2026-01-21 02:47:06 +0530

    signup url created

--- iStoreX/store/urls.py
+++ iStoreX/store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.storeIndexView,name='storeIndex'),
     path('products/<category>',views.storeProductView,name='products'),
-     path("login/", views.login_view, name="login"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
 ]
