0510c05688f31535691f76b332d3e5bd64d6a98a
Tanvi Jivatode <tanvijivatode2711@gmail.com>
2026-01-21 02:33:50 +0530

    modified

--- iStoreX/store/views.py
+++ iStoreX/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . import models
 from .models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
