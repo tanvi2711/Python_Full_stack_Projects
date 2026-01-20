0c75509e51fc12ff987bdc2699f04892cd45fca1
Tanvi Jivatode <tanvijivatode2711@gmail.com>
2026-01-21 02:47:27 +0530

    forgot_pass url created

--- iStoreX/store/urls.py
+++ iStoreX/store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('products/<category>',views.storeProductView,name='products'),
     path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
+    path("forgot-password/", views.forgot_password_view, name="forgot_password"),
 ]
