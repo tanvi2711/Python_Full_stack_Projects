cdee15685af3eea9a86c408a1b92d765eb925836
Tanvi Jivatode <tanvijivatode2711@gmail.com>
2026-01-21 02:19:23 +0530

    updated urls

--- iStoreX/store/urls.py
+++ iStoreX/store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.storeIndexView,name='storeIndex'),
     path('products/<category>',views.storeProductView,name='products'),
+     path("login/", views.login_view, name="login"),
 ]
