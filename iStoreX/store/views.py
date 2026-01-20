248d59c6211f19625672e317bac196ea6af83aa3
Tanvi Jivatode <tanvijivatode2711@gmail.com>
2026-01-21 03:09:08 +0530

    packages import in views.py

--- iStoreX/store/views.py
+++ iStoreX/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from . import models
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 from .models import *
 from django.contrib.auth import authenticate, login
 
