fd53ff4167dc3726657e476861f469dc3d2b46db
Tanvi Jivatode <tanvijivatode2711@gmail.com>
2026-01-21 02:35:42 +0530

    add user variable in login.html

--- iStoreX/store/views.py
+++ iStoreX/store/views.py
@@ -20,3 +20,6 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
