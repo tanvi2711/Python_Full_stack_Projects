35e86040626bc16f9d85a66fcc3af175693a0d00
Tanvi Jivatode <143626918+tanvi2711@users.noreply.github.com>
2026-01-23 23:50:27 +0530

    Delete resume/urls.py

--- resume/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("create/", views.create_resume, name="create_resume"),
-    path("preview/<int:resume_id>/", views.resume_preview, name="resume_preview"),
-]
