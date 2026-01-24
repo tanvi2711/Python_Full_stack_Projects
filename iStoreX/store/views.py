388127293b6a588d082bc3a35372f7641e2ecfe3
tanvi2711 <tanvijivatode2711@gmail.com>
2026-01-25 02:34:38 +0530

    updated

--- iStoreX/store/views.py
+++ iStoreX/store/views.py
@@ -10,14 +10,14 @@ from django.contrib.auth.decorators import login_required
 
 def storeIndexView(request):
     categories=CategoryModelClass.objects.all()
-    products=ProductModelCLass.objects.all()
+    products=ProductModelClass.objects.all()
     return render(request,"index.html",{'categories':categories,'products':products})
 
 @login_required
 def storeProductView(request,category):
     categories=CategoryModelClass.objects.all()
     category_obj=CategoryModelClass.objects.get(name=category)
-    products=ProductModelCLass.objects.filter(category=category_obj.id)
+    products=ProductModelClass.objects.filter(category=category_obj.id)
     return render(request,"products.html",{'products':products,'categories':categories,'category_name':category_obj.name})
 
 def login_view(request):
