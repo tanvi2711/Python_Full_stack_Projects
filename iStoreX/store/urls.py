6af3a33863756f9554b242ff4a2f97d3fd898322
tanvi2711 <tanvijivatode2711@gmail.com>
2026-01-25 02:32:28 +0530

    updated urls.py

--- iStoreX/store/urls.py
+++ iStoreX/store/urls.py
@@ -2,11 +2,14 @@ from django.urls import *
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('',views.storeIndexView,name='storeIndex'),
     path('products/<category>',views.storeProductView,name='products'),
     path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
-    path("forgot-password/", views.forgot_password_view, name="forgot_password"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("profile/", views.profile_view, name="profile"),
+    path("logout/",auth_views.LogoutView.as_view(next_page="storeIndex"),
+    name="logout"),
 ]
