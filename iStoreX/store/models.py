e4b4e246a28991b292e52f7e615c79fba946623e
tanvi2711 <tanvijivatode2711@gmail.com>
2026-01-25 04:36:13 +0530

    updated models.py

--- iStoreX/store/models.py
+++ iStoreX/store/models.py
@@ -1,22 +1,33 @@
-from django.db.models import * 
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
-class CategoryModelClass(Model):
-    id=AutoField(primary_key=True) 
-    name=CharField(max_length=100)
+class CategoryModelClass(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
 
+class ProductModelCLass(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to="uploads/", null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category=models.ForeignKey(CategoryModelClass, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
     
+class CartModelClass(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        ProductModelCLass,
+        on_delete=models.CASCADE
+    )
+    quantity = models.IntegerField(default=1)
 
-class ProductModelClass(Model):
-    id=AutoField(primary_key=True) 
-    name=CharField(max_length=100)
-    image=FileField(upload_to='uploads/')
-    price=CharField(max_length=100)
-    category=ForeignKey(CategoryModelClass,CASCADE,related_name='Products')
-    description=TextField(max_length=300)
-
-class CartModelClass(Model):
-    id=AutoField(primary_key=True) 
-    
+    def __str__(self):
+        return f"{self.user} - {self.product.name}"
