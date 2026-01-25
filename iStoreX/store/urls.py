70ab4cb1192ac264618a6a969b59ab1600debba9
tanvi2711 <tanvijivatode2711@gmail.com>
2026-01-26 02:49:31 +0530

    new url added

--- iStoreX/store/urls.py
+++ iStoreX/store/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("cart/", views.cart_view, name="cart"),
     path("addcart/<int:product_id>/", views.addcart_view, name="addcart"),
     path("removefromcart/<int:cart_item_id>/", views.removefromcart_view, name="removefromcart"),
-    path("billing/", views.billing_view, name="billing"),
-    
+    path('billing/', views.billing_view, name='billing'),
+    path("fake-payment/<int:final_amount>/", views.fake_payment_view, name="fake_payment"),
+    path("payment-success/", views.payment_success_view, name="payment_success"),
 ]
