a6cee339b8ac33ca1b56fa40305e822af226e820
Tanvi Jivatode <143626918+tanvi2711@users.noreply.github.com>
2026-01-28 04:28:41 +0530

    Update settings.py

--- iStoreX/EcomApp/settings.py
+++ iStoreX/EcomApp/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-)8=&nf+vwvef9xs@omm@pp^$y0=2t2kke=&#43+7s3q1+e=b(*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
