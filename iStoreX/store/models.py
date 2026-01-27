a76fa3f1c4b72ae9b058f89903cdc5599a0a76b5
tanvi2711 <tanvijivatode2711@gmail.com>
2026-01-28 03:17:46 +0530

    create requirements.txt

--- iStoreX/store/models.py
+++ iStoreX/store/models.py
@@ -12,7 +12,7 @@ class ProductModelClass(models.Model):
     id=models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to="uploads/", null=True, blank=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category=models.ForeignKey(CategoryModelClass, on_delete=models.CASCADE)
     def __str__(self):
